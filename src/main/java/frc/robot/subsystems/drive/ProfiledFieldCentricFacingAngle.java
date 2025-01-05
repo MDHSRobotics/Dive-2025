eed296d4ffd3c28ec3b0327fb626efe747991c43
Anthony Pagone <anthony21@pagone.com>
2025-01-04 20:16:12 -0800

    Installed SOME vendordeps, created an improved swerve aiming request...
    to allow for smoother movement to the target position (tuning still required).
    Mechanical Advantage did something similar last year by using a ProfiledPIDController:
    https://github.com/Mechanical-Advantage/RobotCode2024/blob/main/src/main/java/org/littletonrobotics/frc2024/subsystems/drive/controllers/HeadingController.java

---
+++ src/main/java/frc/robot/subsystems/drive/ProfiledFieldCentricFacingAngle.java
@@ -0,0 +1,399 @@
+package frc.robot.subsystems.drive;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveControlParameters;
+import com.ctre.phoenix6.swerve.SwerveModule;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.measure.*;
+
+/**
+ * Drives the swerve drivetrain in a field-centric manner, maintaining a
+ * specified heading angle to ensure the robot is facing the desired direction.
+ * Rotation to the target direction is profiled using a trapezoid profile.
+ * <p>
+ * When users use this request, they specify the direction the robot should
+ * travel oriented against the field, and the direction the robot should be facing.
+ * <p>
+ * An example scenario is that the robot is oriented to the east, the VelocityX
+ * is +5 m/s, VelocityY is 0 m/s, and TargetDirection is 180 degrees.
+ * In this scenario, the robot would drive northward at 5 m/s and turn clockwise
+ * to a target of 180 degrees.
+ * <p>
+ * This swerve request is based on {@link com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle FieldCentricFacingAngle},
+ * and makes some other improvements besides the motion profile.
+ * <p>
+ * We opted to take some code from {@link edu.wpi.first.math.controller.ProfiledPIDController ProfiledPIDController}
+ * so we could use a {@link PhoenixPIDController PhoenixPIDController} instead of a normal PID controller.
+ * @see <a href="https://www.chiefdelphi.com/t/implementing-feedforward-with-ctre-s-fieldcentricfacingangle-request/475822/14">Original source of this code</a>
+ */
+public class ProfiledFieldCentricFacingAngle implements SwerveRequest {
+    /**
+     * The velocity in the X direction, in m/s.
+     * X is defined as forward according to WPILib convention,
+     * so this determines how fast to travel forward.
+     */
+    private double velocityX = 0;
+    /**
+     * The velocity in the Y direction, in m/s.
+     * Y is defined as to the left according to WPILib convention,
+     * so this determines how fast to travel to the left.
+     */
+    private double velocityY = 0;
+    /**
+     * The desired direction to face.
+     * 0 Degrees is defined as in the direction of the X axis.
+     * As a result, a TargetDirection of 90 degrees will point along
+     * the Y axis, or to the left.
+     */
+    private Rotation2d targetDirection = new Rotation2d();
+
+    /**
+     * The allowable deadband of the request, in m/s.
+     */
+    private double deadband = 0;
+    /**
+     * The rotational deadband of the request, in radians per second.
+     */
+    private double rotationalDeadband = 0;
+    /**
+     * The center of rotation the robot should rotate around.
+     * This is (0,0) by default, which will rotate around the center of the robot.
+     */
+    private Translation2d centerOfRotation = new Translation2d();
+
+    /**
+     * The type of control request to use for the drive motor.
+     */
+    private SwerveModule.DriveRequestType driveRequestType = SwerveModule.DriveRequestType.OpenLoopVoltage;
+    /**
+     * The type of control request to use for the steer motor.
+     */
+    private SwerveModule.SteerRequestType steerRequestType = SwerveModule.SteerRequestType.Position;
+    /**
+     * Whether to desaturate wheel speeds before applying.
+     * For more information, see the documentation of {@link SwerveDriveKinematics#desaturateWheelSpeeds}.
+     */
+    private boolean desaturateWheelSpeeds = true;
+
+    /**
+     * The perspective to use when determining which direction is forward for driving.
+     * Unlike {@link com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle#ForwardPerspective FieldCentricFacingAngle}, this does NOT influence target direction.
+     */
+    private ForwardPerspectiveValue drivingPerspective = ForwardPerspectiveValue.OperatorPerspective;
+
+    /**
+     * The perspective to use when determing which direction is forward for aiming.
+     * By default, this swerve request considers the angle to be in <a
+     *     href="https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html#always-blue-origin">the blue coordinate system.</a>
+     */
+    private ForwardPerspectiveValue aimingPerspective = ForwardPerspectiveValue.BlueAlliance;
+
+    private final FieldCentric fieldCentric = new FieldCentric();
+
+    private final PhoenixPIDController headingController = new PhoenixPIDController(0, 0, 0);
+
+    /* Profile used for the target direction */
+    private final TrapezoidProfile profile;
+    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+    private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+
+    /**
+     * Creates a new profiled FieldCentricFacingAngle request with the given constraints.
+     *
+     * @param constraints Constraints for the trapezoid profile
+     */
+    public ProfiledFieldCentricFacingAngle(TrapezoidProfile.Constraints constraints) {
+        headingController.enableContinuousInput(-Math.PI, Math.PI);
+        profile = new TrapezoidProfile(constraints);
+    }
+
+    /**
+     * @see edu.wpi.first.math.controller.ProfiledPIDController#calculate(double, double)
+     * @see edu.wpi.first.math.controller.ProfiledPIDController#calculate(double)
+     * @see com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle#apply(SwerveControlParameters, SwerveModule...)
+     */
+    public StatusCode apply(SwerveControlParameters parameters, SwerveModule... modulesToApply) {
+        Rotation2d currentAngle = parameters.currentPose.getRotation();
+        Rotation2d fakeTargetDirection = targetDirection;
+
+        /* If the user requested a target direction according to the operator perspective, rotate our target direction by the angle */
+        if (aimingPerspective == ForwardPerspectiveValue.OperatorPerspective) {
+            fakeTargetDirection = targetDirection.rotateBy(parameters.operatorForwardDirection);
+        }
+
+        /* From ProfiledPIDController#calculate(double)
+         * The following code handles wrapping values (like angles) by eliminating unnecessary rotation.
+         * Basically, if the end goal is more than 180 degrees from the current angle, it moves the goal closer.
+         * The same happens with setpoint.
+         */
+        // Get the smallest possible distance between goal and measurement
+        double goalMinDistance = MathUtil.angleModulus(fakeTargetDirection.getRadians() - currentAngle.getRadians());
+        double setpointMinDistance = MathUtil.angleModulus(setpoint.position - currentAngle.getRadians());
+
+        // Recompute the profile goal with the smallest error, thus giving the shortest path. The goal
+        // may be outside the input range after this operation, but that's OK because the controller
+        // will still go there and report an error of zero. In other words, the setpoint only needs to
+        // be offset from the measurement by the input range modulus; they don't need to be equal.
+        goal.position = goalMinDistance + currentAngle.getRadians();
+        setpoint.position = setpointMinDistance + currentAngle.getRadians();
+
+        // Progress the setpoint of the motion profile
+        setpoint = profile.calculate(parameters.updatePeriod, setpoint, goal);
+
+        // Calculate the extra angular velocity necessary to get the robot to the correct angle.
+        double errorCorrectionOutput =
+                headingController.calculate(currentAngle.getRadians(), setpoint.position, parameters.timestamp);
+
+        double toApplyOmega = setpoint.velocity + errorCorrectionOutput;
+
+        return fieldCentric
+                .withVelocityX(velocityX)
+                .withVelocityY(velocityY)
+                .withRotationalRate(toApplyOmega)
+                .withDeadband(deadband)
+                .withRotationalDeadband(rotationalDeadband)
+                .withCenterOfRotation(centerOfRotation)
+                .withDriveRequestType(driveRequestType)
+                .withSteerRequestType(steerRequestType)
+                .withDesaturateWheelSpeeds(desaturateWheelSpeeds)
+                .withForwardPerspective(drivingPerspective)
+                .apply(parameters, modulesToApply);
+    }
+
+    /**
+     * Resets the profile used for the target direction.
+     *
+     * @param currentHeading The current heading of the robot
+     * @param currentAngularVelocity The current yaw rate of the robot
+     */
+    public void resetProfile(Rotation2d currentHeading, AngularVelocity currentAngularVelocity) {
+        setpoint.position = currentHeading.getRadians();
+        setpoint.velocity = currentAngularVelocity.in(RadiansPerSecond);
+    }
+
+    /**
+     * Modifies the VelocityX parameter and returns itself.
+     * <p>
+     * The velocity in the X direction, in m/s. X is defined as forward according to
+     * WPILib convention, so this determines how fast to travel forward.
+     *
+     * @param newVelocityX Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withVelocityX(double newVelocityX) {
+        this.velocityX = newVelocityX;
+        return this;
+    }
+
+    /**
+     * Modifies the VelocityX parameter and returns itself.
+     * <p>
+     * The velocity in the X direction, in m/s. X is defined as forward according to
+     * WPILib convention, so this determines how fast to travel forward.
+     *
+     * @param newVelocityX Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withVelocityX(LinearVelocity newVelocityX) {
+        this.velocityX = newVelocityX.in(MetersPerSecond);
+        return this;
+    }
+
+    /**
+     * Modifies the VelocityY parameter and returns itself.
+     * <p>
+     * The velocity in the Y direction, in m/s. Y is defined as to the left
+     * according to WPILib convention, so this determines how fast to travel to the
+     * left.
+     *
+     * @param newVelocityY Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withVelocityY(double newVelocityY) {
+        this.velocityY = newVelocityY;
+        return this;
+    }
+
+    /**
+     * Modifies the VelocityY parameter and returns itself.
+     * <p>
+     * The velocity in the Y direction, in m/s. Y is defined as to the left
+     * according to WPILib convention, so this determines how fast to travel to the
+     * left.
+     *
+     * @param newVelocityY Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withVelocityY(LinearVelocity newVelocityY) {
+        this.velocityY = newVelocityY.in(MetersPerSecond);
+        return this;
+    }
+
+    /**
+     * Modifies the TargetDirection parameter and returns itself.
+     * <p>
+     * The desired direction to face. 0 Degrees is defined as in the direction of
+     * the X axis. As a result, a TargetDirection of 90 degrees will point along
+     * the Y axis, or to the left.
+     *
+     * @param newTargetDirection Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withTargetDirection(Rotation2d newTargetDirection) {
+        this.targetDirection = newTargetDirection;
+        return this;
+    }
+
+    /**
+     * Modifies the Deadband parameter and returns itself.
+     * <p>
+     * The allowable deadband of the request, in m/s.
+     *
+     * @param newDeadband Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withDeadband(double newDeadband) {
+        this.deadband = newDeadband;
+        return this;
+    }
+
+    /**
+     * Modifies the Deadband parameter and returns itself.
+     * <p>
+     * The allowable deadband of the request, in m/s.
+     *
+     * @param newDeadband Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withDeadband(LinearVelocity newDeadband) {
+        this.deadband = newDeadband.in(MetersPerSecond);
+        return this;
+    }
+
+    /**
+     * Modifies the RotationalDeadband parameter and returns itself.
+     * <p>
+     * The rotational deadband of the request, in radians per second.
+     *
+     * @param newRotationalDeadband Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withRotationalDeadband(double newRotationalDeadband) {
+        this.rotationalDeadband = newRotationalDeadband;
+        return this;
+    }
+
+    /**
+     * Modifies the RotationalDeadband parameter and returns itself.
+     * <p>
+     * The rotational deadband of the request, in radians per second.
+     *
+     * @param newRotationalDeadband Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withRotationalDeadband(AngularVelocity newRotationalDeadband) {
+        this.rotationalDeadband = newRotationalDeadband.in(RadiansPerSecond);
+        return this;
+    }
+
+    /**
+     * Modifies the CenterOfRotation parameter and returns itself.
+     * <p>
+     * The center of rotation the robot should rotate around. This is (0,0) by
+     * default, which will rotate around the center of the robot.
+     *
+     * @param newCenterOfRotation Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withCenterOfRotation(Translation2d newCenterOfRotation) {
+        this.centerOfRotation = newCenterOfRotation;
+        return this;
+    }
+
+    /**
+     * Modifies the DriveRequestType parameter and returns itself.
+     * <p>
+     * The type of control request to use for the drive motor.
+     *
+     * @param newDriveRequestType Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withDriveRequestType(SwerveModule.DriveRequestType newDriveRequestType) {
+        this.driveRequestType = newDriveRequestType;
+        return this;
+    }
+
+    /**
+     * Modifies the SteerRequestType parameter and returns itself.
+     * <p>
+     * The type of control request to use for the drive motor.
+     *
+     * @param newSteerRequestType Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withSteerRequestType(SwerveModule.SteerRequestType newSteerRequestType) {
+        this.steerRequestType = newSteerRequestType;
+        return this;
+    }
+
+    /**
+     * Modifies the DesaturateWheelSpeeds parameter and returns itself.
+     * <p>
+     * Whether to desaturate wheel speeds before applying. For more information, see
+     * the documentation of {@link SwerveDriveKinematics#desaturateWheelSpeeds}.
+     *
+     * @param newDesaturateWheelSpeeds Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withDesaturateWheelSpeeds(boolean newDesaturateWheelSpeeds) {
+        this.desaturateWheelSpeeds = newDesaturateWheelSpeeds;
+        return this;
+    }
+
+    /**
+     * Modifies the ForwardPerspective parameter and returns itself.
+     * <p>
+     * The perspective to use when determining which direction is forward for driving.
+     *
+     * @param newDrivingPerspective Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withDrivingPerspective(ForwardPerspectiveValue newDrivingPerspective) {
+        this.drivingPerspective = newDrivingPerspective;
+        return this;
+    }
+
+    /**
+     * Modifies the ForwardPerspective parameter and returns itself.
+     * <p>
+     * The perspective to use when determining which direction is forward for aiming.
+     *
+     * @param newAimingPerspective Parameter to modify
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withAimingPerspective(ForwardPerspectiveValue newAimingPerspective) {
+        this.aimingPerspective = newAimingPerspective;
+        return this;
+    }
+
+    /**
+     * Modifies the PID gains for the heading controller and returns itself.
+     *
+     * @param kp The proportional coefficient.
+     * @param ki The integral coefficient.
+     * @param kd The derivative coefficient.
+     * @return this object
+     */
+    public ProfiledFieldCentricFacingAngle withPIDGains(double kp, double ki, double kd) {
+        this.headingController.setPID(kp, ki, kd);
+        return this;
+    }
+}
