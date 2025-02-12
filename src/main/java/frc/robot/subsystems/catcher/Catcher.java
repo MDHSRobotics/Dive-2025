76197e75cdfc42fc7d2d35708f596e7f2638ddae
Anthony Pagone <anthony21@pagone.com>
2025-02-11 17:06:06 -0800

    Reconfigure motor to use rev through bore encoder
    This may or may not be added before comp.

--- src/main/java/frc/robot/subsystems/catcher/Catcher.java
+++ src/main/java/frc/robot/subsystems/catcher/Catcher.java
@@ -46,20 +46,21 @@ public class Catcher extends SubsystemBase {
         SparkFlexConfig armConfig = new SparkFlexConfig();
         armConfig.smartCurrentLimit(CURRENT_LIMIT).idleMode(IdleMode.kBrake);
         armConfig
-                .encoder
+                .absoluteEncoder
                 .positionConversionFactor(ARM_POSITION_CONVERSION_FACTOR)
-                .velocityConversionFactor(ARM_VELOCITY_CONVERSION_FACTOR);
+                .velocityConversionFactor(ARM_VELOCITY_CONVERSION_FACTOR)
+                .averageDepth(2);
         armConfig
                 .closedLoop
-                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
                 .p(K_P)
                 .d(K_D);
         armConfig
                 .signals
-                .primaryEncoderPositionPeriodMs(10)
-                .primaryEncoderPositionAlwaysOn(true)
-                .primaryEncoderVelocityPeriodMs(10)
-                .primaryEncoderVelocityAlwaysOn(true);
+                .absoluteEncoderPositionPeriodMs(10)
+                .absoluteEncoderPositionAlwaysOn(true)
+                .absoluteEncoderVelocityPeriodMs(10)
+                .absoluteEncoderVelocityAlwaysOn(true);
         m_armMotor.configure(armConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         SparkFlexConfig flywheelsConfig = new SparkFlexConfig();
