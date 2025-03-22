75e936deadb1105e6d489c3541a878988be53513
Anthony Pagone <anthony21@pagone.com>
2025-03-22 16:31:03 -0700

    Removed catcher

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,8 +20,6 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.*;
 import frc.robot.commands.AimingRoutines;
 import frc.robot.commands.WheelRadiusCharacterization;
-import frc.robot.subsystems.catcher.Catcher;
-import frc.robot.subsystems.catcher.Catcher.CatcherArmPositions;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 import frc.robot.subsystems.drive.DriveConstants;
@@ -53,7 +51,6 @@ public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final CommandSwerveDrivetrain m_drivetrain = TunerConstants.createDrivetrain();
     private final Climb m_climb = new Climb();
-    private final Catcher m_catcher = new Catcher();
     private final Intake m_intake = new Intake();
 
     private final AutoTimer m_autoTimer = new AutoTimer();
@@ -131,7 +128,6 @@ public class RobotContainer {
                 .withDeadband(getDeadband())
                 .withRotationalDeadband(getRotationalDeadband())));
         m_climb.setDefaultCommand(m_climb.disableMotorsCommand());
-        m_catcher.setDefaultCommand(m_catcher.disableMotorsCommand());
         m_intake.setDefaultCommand(m_intake.disableMotorsCommand());
     }
 
@@ -233,18 +229,8 @@ public class RobotContainer {
                 .whileTrue(m_climb.setPowerCommand(
                         () -> -operatorController.getLeftY(), () -> -operatorController.getLeftY()));
 
-        operatorController.leftStick().toggleOnTrue(m_catcher.setArmPowerCommand(() -> -operatorController.getLeftY()));
         operatorController.rightStick().toggleOnTrue(m_intake.armTestCommand(() -> -operatorController.getRightY()));
 
-        operatorController
-                .leftBumper()
-                .toggleOnTrue(m_catcher.setArmPositionCommand(CatcherArmPositions.CORAL_STATION));
-        operatorController.rightBumper().toggleOnTrue(m_catcher.setArmPositionCommand(CatcherArmPositions.TROUGH));
-
-        operatorController.a().whileTrue(m_catcher.raiseAndRunWheelCommand());
-        operatorController.b().whileTrue(m_catcher.wheelBackwardsCommand());
-
-        operatorController.povLeft().whileTrue(m_catcher.wheelBackwardsWhileRaisingArmCommand());
         operatorController.povDown().toggleOnTrue(m_intake.setArmPositionCommand(IntakeArmPositions.GROUND_PICKUP));
         operatorController.povUp().toggleOnTrue(m_intake.setArmPositionCommand(IntakeArmPositions.ON_CORAL_PICKUP));
         operatorController.povRight().toggleOnTrue(m_intake.setArmPositionCommand(IntakeArmPositions.PROCESSOR));
@@ -252,7 +238,6 @@ public class RobotContainer {
         operatorController.x().whileTrue(m_intake.runWheelsCommand());
         operatorController.y().whileTrue(m_intake.wheelsBackwardsCommand());
 
-        operatorController.back().onTrue(m_catcher.setArmPositionCommand(CatcherArmPositions.STOWED));
         operatorController.start().onTrue(m_intake.setArmPositionCommand(IntakeArmPositions.STOWED));
     }
 
@@ -260,14 +245,6 @@ public class RobotContainer {
      * Registers the <a href="https://pathplanner.dev/pplib-named-commands.html">Named Commands</a> used in PathPlanner.
      */
     private void registerNamedCommands() {
-        NamedCommands.registerCommand(
-                "Lower Catcher Arm", m_catcher.setArmPositionAndEndCommand(CatcherArmPositions.TROUGH));
-        NamedCommands.registerCommand(
-                "Eject Coral", m_catcher.wheelBackwardsCommand().withTimeout(0.5));
-        NamedCommands.registerCommand(
-                "Raise Catcher Arm", m_catcher.setArmPositionAndEndCommand(CatcherArmPositions.CORAL_STATION));
-        NamedCommands.registerCommand(
-                "Intake Coral", m_catcher.runWheelCommand().withTimeout(1));
         NamedCommands.registerCommand("Start Auto Timer", Commands.runOnce(m_autoTimer::resetAndStart));
         NamedCommands.registerCommand("End Auto Timer", Commands.runOnce(m_autoTimer::stopAndPublish));
     }
